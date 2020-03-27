168c5dc56283d75f6a17a6d32fe813b24caec5b4
Thiago <thiagofp707@gmail.com>
2020-03-27 03:06:58 -0300

    Web: Logon layout done

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import './global.css'
+
+import Logon from './pages/Logon';
+
+function App() {
+  return (
+    <div>
+      <Logon></Logon>
+    </div>
+  );
+}
+
+export default App;
